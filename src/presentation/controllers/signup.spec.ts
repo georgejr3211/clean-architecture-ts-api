5d5fe764839339d9aa21a647cbd8c17e12e07005
George Alexandre <gfeitosasolucoes@gmail.com>
2021-02-20 19:08:52 -0300

    refactor: remove email validator with error

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -19,18 +19,6 @@ const makeEmailValidator = (): EmailValidator => {
   return emailValidatorStub
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new InternalServerError()
-    }
-  }
-
-  const emailValidatorStub = new EmailValidatorStub()
-
-  return emailValidatorStub
-}
-
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
@@ -136,8 +124,11 @@ describe('SignUpController', () => {
   })
 
   test('Should return statusCode 500 if EmailValidator throws a error', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
 
     const httpRequest = {
       body: {
